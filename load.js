0f8b30402536f4b349dba37c18d0e2dda221859e
Julius Michaelis <glitter@liftm.de>
2023-10-29 14:37:59 +0900

    Polish

--- load.js
+++ load.js
@@ -1,7 +1,1 @@
-(async () => {
-    await load();
-})();
-
-async function load() {
-	await wasm_bindgen(chrome.runtime.getURL('pkg/ext_bg.wasm'));
-}
+wasm_bindgen(chrome.runtime.getURL('pkg/ext_bg.wasm'));
