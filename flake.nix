11e3ff2ea4c2918aa85ea3b21f5c63f0f5d8321b
Julius Michaelis <gitter@liftm.de>
2025-02-06 21:44:01 +0900

    flake-parts isn't worth it

--- flake.nix
+++ flake.nix
@@ -1,28 +1,26 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem
-    (
-      system: let
-        pkgs = import nixpkgs {inherit system;};
-      in rec {
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [packages.default];
-          buildInputs = with pkgs; [
-            rustfmt
-            cargo-watch
-            rust-analyzer
-          ];
-          CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_LINKER = "lld";
-        };
-        packages.default = pkgs.callPackage ./. {};
-      }
-    );
+  }: let
+    eachSys = f: nixpkgs.lib.mapAttrs (_: pkgs: f pkgs) nixpkgs.legacyPackages;
+  in {
+    devShells = eachSys (pkgs: {
+      default = pkgs.mkShell {
+        inputsFrom = [self.packages.${pkgs.system}.default];
+        buildInputs = with pkgs; [
+          rustfmt
+          cargo-watch
+          rust-analyzer
+        ];
+        CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_LINKER = "lld";
+      };
+    });
+    packages = eachSys (pkgs: {
+      default = pkgs.callPackage ./. {};
+    });
+  };
 }
